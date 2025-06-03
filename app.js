c15539f7ea6a32fac402d9596300e9d27a81cd3f
Ubuntu <ubuntu@ip-172-26-6-108.us-west-2.compute.internal>
2025-06-03 00:17:07 +0000

    add files for docker hosting

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import adminRouter from "./controllers/admin.js";
 import adminChecker from "./middleware/adminChecker.js";
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://upright-connections.click", credentials: true }));
 app.use(express.json());
 app.use("/games", gamesRouter);
 app.use("/users", userRouter);
